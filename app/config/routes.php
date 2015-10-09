99c962a346f0172c14b574cd718d440619e96219
2 Hats <info@2hatslogic.com>
2015-10-09 17:39:39 +0530

    Routing fixes

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,24 +1,24 @@
 <?php
 define('CUSTOM_ROUTES', serialize(array(
 
-   '/register'             =>  'Register:index',
+   '/register(/)'             =>  'Register:index',
     '/register/submit'     =>  'Register:submit',
-    '/login'               =>  'Login:index',
-    '/login/submit'        =>  'Login:submit',
-    '/forgot-password'     =>  'Login:forgot',
-    '/logout'              =>  'Login:logout',
-    '/dashboard'           =>  'Dashboard:index',
-    '/update-info'  	   =>  'Dashboard:updateInfo',
-    '/create-order'        =>  'Order:createOrder',
-    '/create-reorder'      =>  'Order:createReOrder',
+    '/login(/)'               =>  'Login:index',
+    '/login/submit(/)'        =>  'Login:submit',
+    '/forgot-password(/)'     =>  'Login:forgot',
+    '/logout(/)'              =>  'Login:logout',
+    '/dashboard(/)'           =>  'Dashboard:index',
+    '/update-info(/)'  	   =>  'Dashboard:updateInfo',
+    '/create-order(/)'        =>  'Order:createOrder',
+    '/create-reorder(/)'      =>  'Order:createReOrder',
     '/create-reorder2/:id' =>  'Order:createReOrderStepTwo',
-    '/submit-reorder'      =>  'Order:submitReorder',
-    '/confirm-order'       =>  'Order:confirmOrder',
+    '/submit-reorder(/)'      =>  'Order:submitReorder',
+    '/confirm-order(/)'       =>  'Order:confirmOrder',
     // '/create-order/type'   =>  'Order:createOrderStepTwo',
     // '/create-reorder/:id'  =>  'Order:createReOrder',
-    '/order-final'         =>  'Order:confirmFinal',
+    '/order-final(/)'         =>  'Order:confirmFinal',
     '/order-detail/:id'    =>  'Order:orderDetail',
-    '/previous-orders'     =>  'Order:previousOrders',
+    '/previous-orders(/)'     =>  'Order:previousOrders',
     
 )));
  
