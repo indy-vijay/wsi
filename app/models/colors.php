bfd64ddb4043f1f421ed6b05f67cf35261327c51
= <=>
2016-03-30 16:37:46 +0530

    Blank colors error fix

--- app/models/colors.php
+++ app/models/colors.php
@@ -1,23 +1,28 @@
 <?php
 use Illuminate\Database\Eloquent\Model as Eloquent;
 
-class Colors extends Eloquent 
+class Colors extends Eloquent
 {
     // public $table = 'brands';
-	protected $fillable    = array('id','styles_id','color');
-    public    $timestamps   = false;
+    protected $fillable = array('id', 'styles_id', 'color');
+    public $timestamps = false;
 
     public function scopeName($query, $id)
     {
-        return $query->where('id',$id)
-                        ->select('color')
-                        ->first()
-                        ->toArray()['color'];
+        $result = $query->where('id', $id)
+            ->select('color')
+            ->first();
+
+        if (count($result) == 1) {
+            return $result->toArray()['color'];
+        }
+
+        return 'N/A';
     }
 
     public function scopeSelectedColor($query, $style)
     {
-        return $query->where('style',$style);
+        return $query->where('style', $style);
 
     }
 }
